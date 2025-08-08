60100bf9c96cf827983dd547dca8376c1ca5de18
AlphaBeast97 <ksaad5272@gmail.com>
2025-08-08 21:40:58 +0500

    all the routes setup

---
+++ relay/src/components/SideBar.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { SideBarLinks } from "@/constants";
+import { cn } from "@/lib/utils";
+import Link from "next/dist/client/link";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+const SideBar = () => {
+  const pathName = usePathname();
+  return (
+    <section className="sticky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 max-sm:hidden lg:2-[264px]">
+      <div className="flex flex-1 flex-col gap-6">
+        {SideBarLinks.map((link) => {
+          const isActive =
+            pathName === link.route || pathName.startsWith(link.route);
+          return (
+            <Link
+              key={link.label}
+              href={link.route}
+              className={cn(
+                "flex gap-4 items-center p-4 rounded-lg justify-start",
+                {
+                  "bg-blue-1": isActive,
+                }
+              )}
+            >
+              <Image width={24} height={24} src={link.imgUrl} alt={link.label} className="text-lg font-semibold max-lg:hidden"/>
+              <span>{link.label}</span>
+            </Link>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default SideBar;
