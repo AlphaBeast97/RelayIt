54b11fe6a4a0b599842b3ae048fd64fff5ff75c9
AlphaBeast97 <ksaad5272@gmail.com>
2025-08-11 17:41:42 +0500

    feat: Added Modal

--- relay/src/components/MeetingTypeList.tsx
+++ relay/src/components/MeetingTypeList.tsx
@@ -2,12 +2,16 @@
 import React, { useState } from "react";
 import HomeCard from "./HomeCard";
 import { useRouter } from "next/navigation";
+import MeetingModel from "./MeetingModel";
 
 const MeetingTypeList = () => {
   const [meetingState, setMeetingState] = useState<
     "isScheduleMeeting" | "isJoiningMeeting" | "isInstantMeeting" | undefined
   >();
   const router = useRouter();
+  const createMeeting = () => {
+    // Logic to create a meeting
+  };
   return (
     <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
       <HomeCard
@@ -38,6 +42,18 @@ const MeetingTypeList = () => {
         handleClick={() => setMeetingState("isJoiningMeeting")}
         className="bg-yellow-1"
       />
+      <MeetingModel
+        isOpen={meetingState === "isInstantMeeting"}
+        onclose={() => setMeetingState(undefined)}
+        title="Start an Instant Meeting"
+        className="text-center"
+        buttonText="Start Meeting"
+        handleClick={() => createMeeting()}
+        image=""
+        buttonIcon=""
+      >
+        <p>Start your meeting instantly with a single click.</p>
+      </MeetingModel>
     </section>
   );
 };
